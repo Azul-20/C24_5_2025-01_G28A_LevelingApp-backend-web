7689a6aab80ca21ae6171d9f45c8d3fc47c6e319
Azul-20 <rael.rivero01@gmail.com>
2025-07-12 06:06:30 -0500

    index.js versión 1.0

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require('./routes/auth');
+app.use('/api', authRoutes);
+
+// Rutas protegidas
+const authMiddleware = require('./middlewares/authMiddleware');
+app.get('/api/perfil', authMiddleware, (req, res) => {
+  res.json({ mensaje: 'Ruta protegida', usuario: req.user });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor en http://localhost:${PORT}`));
