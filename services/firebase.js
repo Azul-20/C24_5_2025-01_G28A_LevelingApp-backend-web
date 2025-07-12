c338eef441f23e152650e2026ddbb467cab98991
Azul-20 <rael.rivero01@gmail.com>
2025-07-12 07:47:00 -0500

    Correcciones a la conexión de firebase

--- services/firebase.js
+++ services/firebase.js
@@ -1,19 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
+const admin = require('firebase-admin');
+const serviceAccount = require('../path/to/serviceAccountKey.json');
 
-const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.FIREBASE_APP_ID,
-};
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
-// Firebase inicializado
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-
-export { app, auth };
+module.exports = admin;
